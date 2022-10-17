cff8a34d413a5f3d034f678186b07771dc4ec693
Jawagar <114825551+Jawagar27@users.noreply.github.com>
2022-10-17 13:43:06 +0530

    Create trigger.java
    Added the file to trigger or implement the stack function

---
+++ DSA/Stacks/trigger.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+	Stack value = new Stack();
+
+		value.push();
+		value.pop();
+		value.peek();
+		value.display();
+
+		
+	}
+
+}
