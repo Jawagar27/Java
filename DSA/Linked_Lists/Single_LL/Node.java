98370b5ff8d73268e140fe338d5dd7b2f98d53cd
Jawagar <114825551+Jawagar27@users.noreply.github.com>
2022-10-17 13:57:33 +0530

    Create Node.java
    Added files to make the list node

---
+++ DSA/Linked_Lists/Single_LL/Node.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Node {
+
+	//it must have the index value
+	
+	 Object data;
+	 Node next;
+	
+	public Node() {
+		
+	}
+	
+	public Node(Object data) {
+		super();
+		this.data = data;
+		this.next = null;
+	}
+	
+}
