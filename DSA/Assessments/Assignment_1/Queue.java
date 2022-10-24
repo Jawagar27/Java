45b6dafbaa18e0a801d8c77ea10d9ef7eaa01957
Jawagar <114825551+Jawagar27@users.noreply.github.com>
2022-10-24 23:08:04 +0530

    Added the code
    Added the submitted assignment code, report and text files

---
+++ DSA/Assessments/Assignment_1/Queue.java
@@ -0,0 +1,39 @@
+
+
+public class Queue {
+/* Comp 1002 Assignment 1
+     * Author - Jawagar Prabaharan
+     * Curtin id - 20944793
+     * Queue code */
+    private DSA_LinkedList DSAQueue = new DSA_LinkedList();
+
+    public void enqueue(Object pVal)
+    {
+        DSAQueue.insertLast(pVal);
+    }
+
+    public Object dequeue()
+    {
+        Object returnVal = null;
+        try
+        {
+            DSAQueue.peekFirst();
+            returnVal = DSAQueue.removeFirst();
+        }
+        catch (Exception e)
+        {
+            returnVal = "Nothing to dequeue!";
+        }
+        return returnVal;
+    }
+
+    public boolean isEmpty()
+    {
+        boolean result = false;
+        if (DSAQueue.isEmpty())
+        {
+            result = true;
+        }
+        return result;
+    }
+}
--- DSA/Assessments/Assignment_1/Readme.txt
+++ DSA/Assessments/Assignment_1/Readme.txt
@@ -8,5 +8,3 @@ The other files used are :
 		4.Queue
 
 keyMeUp.java file could be run in interactive mode and silent mode
-
-this code just has implemented some of the functions and other functions are yet to be implemented and the task is to implement them
